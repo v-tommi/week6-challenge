31c9103087f3114000c9f0e74bf3d84c1f648397
v-tommi <tom.miyahira@outlook.com>
2023-01-11 16:39:35 -1000

    update html and js files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,37 +1,17 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-const colorcode =  document.querySelector('.colorcode');
-let currenthour = new Date().getHours();
+$(document).foundation();
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-  if (currenthour === parseInt(document.getElementById(currenthour).innerText)) {
-    colorcode[currenthour].style.backgroundColor = 'blue'
+const creds = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'qBFgx-BuuVc5NkZTKQCBYzhaYm3CAgBfniTZy0Um3DpdKg7z2SBb0GFkD_oh_zUI-4Zi8PdOiFZCwCHLw61efLuFZFmvLjFk8K6wouulsUlxuCgjU8yi6L2HzLu8Y3Yx'
   }
+};
 
+const input = $(document).search
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  var today = dayjs();
-$('#currentDay').text(today.format('MMMM D, YYYY'));
-});
-
-
+fetch('https://api.yelp.com/v3/businesses/search?location=' + input + '&term=restaurants&radius=40000&attributes=&sort_by=best_match&limit=20', creds)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
